156e3787b127a5a4eb7ca09330ea8ff7ae421b18
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-01-19 15:25:55 -0300

    Update validaIEPB.js

--- valida ie/validaIEPB.js
+++ valida ie/validaIEPB.js
@@ -5,7 +5,34 @@ function validaIEPB() {
         console.error('A inscrição estadual da Paraíba  deve conter 9 dígitos.');
         return null;
     }
-
 }
+
+const peso = [9,8,7,6,5,4,3,2];
+const ie = "06000001";
+const ie2 = [];
+
+
+
+for (let i = 0; i < 8; i++) {
+  ie2.push(parseInt(ie[i]) * peso[i]);
+}
+
+
+const somaProdutos1 = ie2.reduce(function (acumulador, valor) {
+  return acumulador + valor;
+}, 0);
+
+
+const resto = somaProdutos1 % 11;
+
+let digitoVerificador;
+if (resto === 10 || resto === 11) {
+  digitoVerificador = 0;
+} else {
+  digitoVerificador = 11 - resto;
+}
+
+console.table(ie2)
+console.log(resto)
 
 export default validaIEPB
