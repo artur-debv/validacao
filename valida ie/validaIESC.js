2ab862d8d153452aec9babc61758500a6375429f
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-01-22 14:59:19 -0300

    Update validaIESC.js

--- valida ie/validaIESC.js
+++ valida ie/validaIESC.js
@@ -6,6 +6,32 @@ function validaIESC() {
         return null;
     }
 
+    const peso = [9,8,7,6,5,4,3,2];
+    const ie = "25104085";
+    const ie2 = [];
+
+   
+    for (let i = 0; i < 8; i++) {
+    ie2.push(parseInt(ie[i]) * peso[i]);
+    }
+
+
+    const somaProdutos1 = ie2.reduce(function (acumulador, valor) {
+     return acumulador + valor;
+    }, 0);
+
+    const resto = somaProdutos1 % 11;
+
+
+    let digitoVerificador;
+    if (resto === 0 || resto === 1) {
+     digitoVerificador = 0;
+    } else {
+     digitoVerificador = 11 - resto;
+    }
+
+console.log(digitoVerificador)
+
 }
 
 export default validaIESC
