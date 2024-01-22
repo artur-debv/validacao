c428ccfe7527442abcb0d47443e7b4d9439befe3
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-01-22 14:54:58 -0300

    Update validaIERS.js

--- valida ie/validaIERS.js
+++ valida ie/validaIERS.js
@@ -5,6 +5,34 @@ function validaIERS() {
         console.error('A inscrição estadual do Rio Grande do Sul deve conter 10 dígitos.');
         return null;
     }
+
+    const peso = [2, 9, 8, 7, 6, 5, 4, 3, 2];
+    const ie = "224365879";
+    const ie2 = [];
+
+    
+    for (let i = 0; i < 9; i++) {
+     ie2.push(parseInt(ie[i]) * peso[i]);
+    }
+
+
+    const somaProdutos1 = ie2.reduce(function (acumulador, valor) {
+     return acumulador + valor;
+    }, 0);
+
+    const resto = somaProdutos1 % 11;
+
+
+    let digitoVerificador;
+    if (resto === 10 || resto === 11) {
+     digitoVerificador = 0;
+    } else {
+     digitoVerificador = 11 - resto;
+    }
+
+    console.table(ie2)
+    console.log(digitoVerificador)
+
 }
 
 export default validaIERS
