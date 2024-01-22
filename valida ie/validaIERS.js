d5b3414f93496ee5d49db8b79948bb2b83869cbb
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-22 16:12:12 -0300

    update validacoes

--- valida ie/validaIERS.js
+++ valida ie/validaIERS.js
@@ -1,7 +1,7 @@
 function validaIERS() {
 
-     // Verificar se a inscrição tem o tamanho esperado
-     if (inscricao.length !== 9) {
+    // Verificar se a inscrição tem o tamanho esperado
+    if (inscricao.length !== 9) {
         console.error('A inscrição estadual do Rio Grande do Sul deve conter 10 dígitos.');
         return null;
     }
@@ -10,14 +10,14 @@ function validaIERS() {
     const ie = "2243658792";
     const ie2 = [];
 
-    
+
     for (let i = 0; i < 9; i++) {
-     ie2.push(parseInt(ie[i]) * peso[i]);
+        ie2.push(parseInt(ie[i]) * peso[i]);
     }
 
 
     const somaProdutos1 = ie2.reduce(function (acumulador, valor) {
-     return acumulador + valor;
+        return acumulador + valor;
     }, 0);
 
     const resto = somaProdutos1 % 11;
@@ -25,19 +25,17 @@ function validaIERS() {
 
     let digitoVerificador;
     if (resto === 10 || resto === 11) {
-     digitoVerificador = 0;
+        digitoVerificador = 0;
     } else {
-     digitoVerificador = 11 - resto;
+        digitoVerificador = 11 - resto;
     }
 
     console.table(ie2)
     console.log(digitoVerificador)
 
-    const digitoverificador = resto.charAt(1)
-   
-    if(ie[9]== digitoverificador){
+    if (ie[9] == digitoverificador) {
         return true
-    }else{
+    } else {
         return false
     }
 
