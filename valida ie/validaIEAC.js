7c3586ad897a5b9c401b9a7054ce41b0e1b7131b
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-01-22 15:04:28 -0300

    Update validaIEAC.js

--- valida ie/validaIEAC.js
+++ valida ie/validaIEAC.js
@@ -6,6 +6,65 @@ function validaIEAC() {
         return null;
     }
 
+    // primeiro cálculo
+
+    const peso = [4,3,2,9,8,7,6,5,4,3,2];
+    const ie = "01004823001";
+    const ie2 = [];
+
+
+    for (let i = 0; i < 11; i++) {
+     ie2.push(parseInt(ie[i]) * peso[i]);
+    }
+
+
+    const somaProdutos1 = ie2.reduce(function (acumulador, valor) {
+    return acumulador + valor;
+    }, 0);
+
+    const resto = somaProdutos1 % 11;
+
+
+    let digitoVerificador;
+    if (resto === 10 || resto === 11) {
+     digitoVerificador = 0;
+    } else {
+     digitoVerificador = 11 - resto;
+    }
+
+    console.table(ie2)
+    console.log(resto)
+    console.log(digitoVerificador)
+
+
+    // segundo cálculo 
+
+    const peso1 = [5,4,3,2,9,8,7,6,5,4,3,2];
+    const ie1 = "010048230011";
+    const ie3 = [];
+
+    for (let i = 0; i < 12; i++) {
+     ie3.push(parseInt(ie1[i]) * peso1[i]);
+    }
+  
+  
+    const somaProdutos2 = ie3.reduce(function (acumulador, valor) {
+     return acumulador + valor;
+    }, 0);
+  
+    const resto2 = somaProdutos2 % 11;
+  
+    let digitoVerificador1;
+    if (resto2 === 10 || resto2 === 11) {
+     digitoVerificador1 = 0;
+     } else {
+     digitoVerificador1 = 11 - resto2;
+    }
+
+    console.table(ie3)
+    console.log(resto2)
+    console.log(digitoVerificador1)
+
 }
 
 export default validaIEAC
