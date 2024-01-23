239827ef4d3690b9b2f82a60ff474f78af3db886
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-01-23 15:39:30 -0300

    Update validaIEMT.js

--- valida ie/validaIEMT.js
+++ valida ie/validaIEMT.js
@@ -1,9 +1,37 @@
+
+let inscricao = "0013000001-9"
+
 inscricao = inscricao.replace(/\D/g, '');
 
 const validainscricao = /^[0-9]{11}$/.test(inscricao);
 
 if (validainscricao) {
-    true
+
+    const peso = [3,2,9,8,7,6,5,4,3,2];
+    const ie = [];
+
+
+    for (let i = 0; i < 10; i++) {
+     ie.push(parseInt(inscricao[i]) * peso[i]);
+    }
+
+
+    const somaProdutos = ie.reduce(function (acumulador, valor) {
+    return acumulador + valor;
+    }, 0);
+
+    const resto = somaProdutos % 11;
+
+    let digitoVerificador
+    if (resto === 0 || resto === 1) {
+        digitoVerificador = 0;
+    } else {
+        digitoVerificador = 11 - resto;
+    }
+
+    console.table(ie)
+    console.log(resto)
+    console.log(somaProdutos)
 } else {
     false
 }
