4c415226dcdd33d9d23dd1bb8ed8193794f37fa0
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-01-23 15:02:47 -0300

    Update validaIEAL.js

--- valida ie/validaIEAL.js
+++ valida ie/validaIEAL.js
@@ -1,9 +1,38 @@
+
+let inscricao = "2400000-4"
+
 inscricao = inscricao.replace(/\D/g, '');
 
 const validainscricao = /^[0-9]{9}$/.test(inscricao);
 
 if (validainscricao) {
-    true
+    const peso = [9,8,7,6,5,4,3,2];
+    const ie = []
+    
+    for (let i = 0; i < 8; i++) {
+      ie.push(parseInt(inscricao[i]) * peso[i]);
+    }
+    
+    
+    const somaProdutos = ie.reduce(function (acumulador, valor) {
+      return acumulador + valor;
+    }, 0);
+    
+    
+    const produto = somaProdutos * 10
+    const resto = produto % 11;
+    
+    
+    if (resto === 10) {
+        digitoVerificador = 0;
+      } else {
+        digitoVerificador = resto;
+      }
+    
+      console.table(ie2)
+      console.log(produto)
+      console.log(resto)
+    
 } else {
     false
 }
