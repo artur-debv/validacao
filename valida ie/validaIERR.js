592c8a0c1e161e87371aed43711f4dc040a19668
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-01-23 16:04:41 -0300

    Update validaIERR.js

--- valida ie/validaIERR.js
+++ valida ie/validaIERR.js
@@ -1,9 +1,30 @@
+
+let inscricao = "24006153-6"
+
 inscricao = inscricao.replace(/\D/g, '');
 
 const validainscricao = /^[0-9]{9}$/.test(inscricao);
 
 if (validainscricao) {
-    true
+    const peso = [1,2,3,4,5,6,7,8];
+    const ie = []
+
+
+    for (let i = 0; i < 8; i++) {
+     ie.push(parseInt(inscricao[i]) * peso[i]);
+    }
+
+
+    const somaProdutos = ie.reduce(function (acumulador, valor) {
+    return acumulador + valor;
+    }, 0);
+
+    const resto = somaProdutos % 9;
+
+    console.log(resto)
+    console.log(somaProdutos)
+    console.table(ie2)
+
 } else {
     false
 }
