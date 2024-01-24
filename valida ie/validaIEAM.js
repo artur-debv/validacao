30cbde35fb29ef027ab83739f611e6dac59f2379
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-24 09:52:07 -0300

    Update validaIEAM.js

--- valida ie/validaIEAM.js
+++ valida ie/validaIEAM.js
@@ -1,9 +1,34 @@
+let inscricao = "99.999.999-9";
+
 inscricao = inscricao.replace(/\D/g, '');
 
 const validainscricao = /^[0-9]{9}$/.test(inscricao);
 
 if (validainscricao) {
-    true
+    const peso = [9, 8, 7, 6, 5, 4, 3, 2];
+    const ie = [];
+
+    for (let i = 0; i < 8; i++) {
+        ie.push(parseInt(inscricao[i]) * peso[i]);
+    }
+
+    const somaProdutos = ie.reduce(function (acumulador, valor) {
+        return acumulador + valor;
+    }, 0);
+
+    if (somaProdutos < 11){
+        digitoVerificador = 11 - somaProdutos;
+    } else {
+        resto = somaProdutos % 11;
+        if (resto <= 1){
+            digitoVerificador = 0;
+        } else {
+            digitoVerificador = 11 - resto
+        }
+    }
+
+    console.table(digitoVerificador)
+
 } else {
     false
 }
