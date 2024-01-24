34c641ffb80069f0a6a96459ef3043a01dad3334
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-24 16:39:04 -0300

    Update validaIERR.js

--- valida ie/validaIERR.js
+++ valida ie/validaIERR.js
@@ -6,30 +6,27 @@ inscricao = inscricao.replace(/\D/g, '');
 const validainscricao = /^[0-9]{9}$/.test(inscricao);
 
 if (validainscricao) {
-    const peso = [1,2,3,4,5,6,7,8];
+    const peso = [1, 2, 3, 4, 5, 6, 7, 8];
     const ie = []
 
-
     for (let i = 0; i < 8; i++) {
-     ie.push(parseInt(inscricao[i]) * peso[i]);
+        ie.push(parseInt(inscricao[i]) * peso[i]);
     }
 
-
     const somaProdutos = ie.reduce(function (acumulador, valor) {
-    return acumulador + valor;
+        return acumulador + valor;
     }, 0);
 
     const resto = somaProdutos % 9;
 
-    console.log(resto)
-    console.log(somaProdutos)
-    console.table(ie2)
+    digitoVerificador = resto
 
-    let digitoVerificador
-    if(ie[8] == digitoVerificador){
-       return true
-    }else{
-        return false
+    if (inscricao[8] == digitoVerificador) {
+        //console.log('sim')
+        true
+    } else {
+        //console.log('não')
+        false
     }
 
 } else {
