7deb931528c10f9aad7cf2e8f773b87d95c2778e
Artur Huber <97331300+artur-debv@users.noreply.github.com>
2024-01-24 15:24:50 -0300

    Update validaIERO.js

--- valida ie/validaIERO.js
+++ valida ie/validaIERO.js
@@ -6,6 +6,31 @@ if (inscricao.length === 9) {
     const validainscricao = /^[0-9]{9}$/.test(inscricao);
 
     if (validainscricao) {
+        const inscricaoSlice = inscricao.slice(3, 8); 
+        const peso = [6,5,4,3,2];
+        const ie = [];
+
+        for (let i = 0; i < 5; i++) {
+        ie.push(parseInt(inscricaoSlice[i]) * peso[i]);
+    }
+
+    const somaProdutos = ie.reduce(function (acumulador, valor) {
+        return acumulador + valor;
+    }, 0);
+
+    const resto = somaProdutos % 11;
+
+    let digitoVerificador;
+    if (resto === 10 || resto === 11) {
+       digitoVerificador = 0;
+    } else {
+    digitoVerificador = 11 - resto;
+    }
+
+    console.table(ie)
+    console.log(somaProdutos)
+    console.log(resto)
+    console.log(digitoVerificador)
 
     } else {
         console.log("Inscrição inválida");
@@ -39,7 +64,6 @@ if (inscricao.length === 9) {
         console.log(somaProdutos)
         console.log(resto)
         console.log(digitoVerificador)
-    
     } else {
        false
     }
