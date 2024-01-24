45790954ed30810e793ede553fccd429f98fbdf1
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-24 16:53:16 -0300

    Update validaIETO.js

--- valida ie/validaIETO.js
+++ valida ie/validaIETO.js
@@ -5,31 +5,34 @@ inscricao = inscricao.replace(/\D/g, '');
 const validainscricao = /^[0-9]{11}$/.test(inscricao);
 
 if (validainscricao) {
-    let inscricaoSlice = inscricao.slice(0, 2) + inscricao.slice(4, 10);
+  let inscricaoSlice = inscricao.slice(0, 2) + inscricao.slice(4, 10);
 
-    const peso = [9, 8, 7, 6, 5, 4, 3, 2];
+  const peso = [9, 8, 7, 6, 5, 4, 3, 2];
 
-    const ie2 = [];
+  const ie = [];
 
-    for (let i = 0; i < 8; i++) {
-     ie2.push(parseInt(inscricaoSlice[i]) * peso[i]);
-    }
+  for (let i = 0; i < 8; i++) {
+    ie.push(parseInt(inscricaoSlice[i]) * peso[i]);
+  }
 
-    const somaProdutos = ie2.reduce((acumulador, valor) => acumulador + valor, 0);
+  const somaProdutos = ie.reduce((acumulador, valor) => acumulador + valor, 0);
 
-    const resto = somaProdutos % 11;
+  const resto = somaProdutos % 11;
 
-    let digitoVerificador;
-
-    if (resto < 2) {
+  if (resto < 2) {
     digitoVerificador = 0;
-    } else {
-  digitoVerificador = 11 - resto;
-    }
+  } else {
+    digitoVerificador = 11 - resto;
+  }
 
-console.log(resto)
-console.log(digitoVerificador)
-console.table(ie2)
-} else {
+  if (inscricao[10] == digitoVerificador) {
+    //console.log('sim')
+    true
+  } else {
+    //console.log('não')
     false
+  }
+
+} else {
+  false
 }
