c573ec447157f48b97fda8f540c3decb3ffb7d32
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-26 11:14:07 -0300

    Update validaIESP.js

--- valida ie/validaIESP.js
+++ valida ie/validaIESP.js
@@ -1,107 +1,110 @@
-function validaIESP() {
+let inscricao = "";
+
+inscricao = inscricao.replace(/\D/g, '');
+
+if (inscricao.length === 12) {
+  const validainscricao = /^[0-9]{12}$/.test(inscricao);
+
+  if (validainscricao) {
+    // primeiro cálculo //
+
+    const pesos1 = [1, 3, 4, 5, 6, 7, 8, 10];
+
+    let ie = "110042490114";
+    let ie2 = [];
 
 
-  // Verificar se a inscrição tem o tamanho esperado
-  if (inscricao.length !== 11) {
-     console.error('A inscrição estadual de São Paulo deve conter 12 dígitos.');
-     return null;
- }
+    for (let i = 0; i < 8; i++) {
+      ie2.push(parseInt(ie[i]) * pesos1[i]);
+    }
 
- if(inscricao.length == 12){
+    console.table(ie2);
 
-      // primeiro cálculo //
+    const somaProdutos1 = ie2.reduce(function (acumulador, valor) {
+      return acumulador + valor;
+    }, 0)
 
-const pesos1 = [1, 3, 4, 5, 6, 7, 8, 10];
+    const resto = somaProdutos1 % 11;
 
-let ie = "110042490114";
-let ie2 = [];
+    const restoString = resto.toString()
 
-
-for (let i = 0; i < 8; i++) {
-ie2.push(parseInt(ie[i]) * pesos1[i]);
-}
-
-console.table(ie2);
-
-const somaProdutos1 = ie2.reduce(function(acumulador,valor){
-  return acumulador + valor;
-},0)
-
-const resto = somaProdutos1 % 11;
-
-const restoString = resto.toString()
-
-console.log(restoString.charAt(1));
+    console.log(restoString.charAt(1));
 
 
-// segundo cálculo
+    // segundo cálculo
 
-let ie3 = [];
+    let ie3 = [];
 
-const pesos2 = [3,2,10,9,8,7,6,5,4,3,2];
+    const pesos2 = [3, 2, 10, 9, 8, 7, 6, 5, 4, 3, 2];
 
-for (let i = 0; i < 11; i++) {
-ie3.push(parseInt(ie[i], 10) * pesos2[i]);
-}
+    for (let i = 0; i < 11; i++) {
+      ie3.push(parseInt(ie[i], 10) * pesos2[i]);
+    }
 
-const somaProdutosb = ie2.reduce(function(acumulador,valor){
-  return acumulador + valor;
-},0)
+    const somaProdutosb = ie2.reduce(function (acumulador, valor) {
+      return acumulador + valor;
+    }, 0)
 
-const resto2 = somaProdutosb % 11;
+    const resto2 = somaProdutosb % 11;
 
-const restoString1 = resto2.toString()
+    const restoString1 = resto2.toString()
 
-console.log(restoString1)
-
-
-const digitoverificador1 = resto.charAt(1)
-const digitoverificador2 = resto.charAt(1)
-
-if(ie[8] == digitoverificador1 && ie[4] == digitoverificador2){
-  return true
-}else{
-  return false
-}
- 
-
-}else if(isncricao.lenght == 13){
-
-// terceiro cálculo  //
-
-const pesos3 = [1,3,4,5,6,7,8,10];
-
-let ieb = "01100424002";
-let ie4 = [];
-
-for (let i = 0; i < 8; i++) {
-ie4.push(parseInt(ieb[i]) * pesos3[i]);
-}
-
-console.table(ie4);
-
-const somaProdutosc = ie4.reduce(function (acumulador, valor) {
-return acumulador + valor;
-}, 0);
-
-const resto3 = somaProdutosc % 11;
-
-const restoString3 = resto3.toString()
-
-console.log(restoString3)
+    console.log(restoString1)
 
 
-}else{
-return false
-}
+    const digitoverificador1 = resto.charAt(1)
+    const digitoverificador2 = resto.charAt(1)
 
-const digitoverificador3 = resto.charAt(1)
+    if (ie[8] == digitoverificador1 && ie[4] == digitoverificador2) {
+      return true
+    } else {
+      return false
+    }
+
+  } else {
+    console.log("Inscrição inválida");
+  }
+
+} else if (inscricao.length === 13) {
+  const validainscricao = /^[0-9]{13}$/.test(inscricao);
+
+  if (validainscricao) {
+    // primeiro cálculo  //
+
+    const pesos3 = [1, 3, 4, 5, 6, 7, 8, 10];
+
+    let ieb = "01100424002";
+    let ie4 = [];
+
+    for (let i = 0; i < 8; i++) {
+      ie4.push(parseInt(ieb[i]) * pesos3[i]);
+    }
+
+    console.table(ie4);
+
+    const somaProdutosc = ie4.reduce(function (acumulador, valor) {
+      return acumulador + valor;
+    }, 0);
+
+    const resto3 = somaProdutosc % 11;
+
+    const restoString3 = resto3.toString()
+
+    console.log(restoString3)
+
+    const digitoverificador3 = resto.charAt(1)
 
 
-if(ie[8] == digitoverificador3){
-  return true
-}else{
-  return false
-}
+    if (ie[8] == digitoverificador3) {
+      return true
+    } else {
+      return false
+    }
 
+  } else {
+    false
+  }
+
+} else {
+  false
 }
