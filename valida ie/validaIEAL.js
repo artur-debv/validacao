a7c5b859861e3a739deb5e544f55d49b3bd3fb41
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-30 11:17:37 -0300

    Update validaIEAL.js

--- valida ie/validaIEAL.js
+++ valida ie/validaIEAL.js
@@ -1,47 +1,51 @@
+function validaIEAL() {
 
-let inscricao = "2400000-48"
+  inscricao = inscricao.replace(/\D/g, '');
 
-inscricao = inscricao.replace(/\D/g, '');
+  const validainscricao = /^[0-9]{9}$/.test(inscricao);
 
-const validainscricao = /^[0-9]{9}$/.test(inscricao);
+  if (validainscricao) {
 
-if (validainscricao) {
+    if (inscricao[0] == 2 && inscricao[1] == 4) {
 
-  if (inscricao[0] == 2 && inscricao[1] == 4) {
-    const peso = [9, 8, 7, 6, 5, 4, 3, 2];
-    const ie = []
+      const peso = [9, 8, 7, 6, 5, 4, 3, 2];
+      const ie = []
 
-    for (let i = 0; i < 8; i++) {
-      ie.push(parseInt(inscricao[i]) * peso[i]);
-    }
+      for (let i = 0; i < 8; i++) {
+        ie.push(parseInt(inscricao[i]) * peso[i]);
+      }
 
-    const somaProdutos = ie.reduce(function (acumulador, valor) {
-      return acumulador + valor;
-    }, 0);
+      const somaProdutos = ie.reduce(function (acumulador, valor) {
+        return acumulador + valor;
+      }, 0);
 
-    const produto = somaProdutos * 10
-    const resto = produto % 11;
+      const produto = somaProdutos * 10
 
-    if (resto === 10) {
-      digitoVerificador = 0;
+      const resto = produto % 11;
+
+      if (resto === 10) {
+        digitoVerificador = 0;
+
+      } else {
+        digitoVerificador = resto;
+      }
+
+      if (inscricao[8] == digitoVerificador) {
+        return true
+
+      } else {
+        return false
+      }
+
     } else {
-      digitoVerificador = resto;
+      return false
     }
 
-    if (inscricao[8] == digitoVerificador) {
-      //console.log('sim')
-      true
-    } else {
-      //console.log('não')
-      false
-    }
   } else {
-    false
+    return false
   }
 
-} else {
-  false
 }
 
-
-
+//let inscricao = "2400000-48"
+//console.log(validaIEAL(inscricao))
