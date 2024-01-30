04a8b5e919edf392557156310c4fc34873bcb0ee
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-30 11:17:24 -0300

    Update validaIEAC.js

--- valida ie/validaIEAC.js
+++ valida ie/validaIEAC.js
@@ -1,67 +1,69 @@
-let inscricao = "01.004.823/001-12";
+function validaIEAC() {
 
-inscricao = inscricao.replace(/\D/g, '');
+    inscricao = inscricao.replace(/\D/g, '');
 
-const validainscricao = /^[0-9]{13}$/.test(inscricao);
+    const validainscricao = /^[0-9]{13}$/.test(inscricao);
 
-if (validainscricao) {
+    if (validainscricao) {
 
-    if (inscricao[0] == 0 && inscricao[1] == 1) {
-        // primeiro cálculo
+        if (inscricao[0] == 0 && inscricao[1] == 1) {
 
-        const peso = [4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
-        const ie = [];
+            // primeiro cálculo
+            const peso = [4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+            const ie = [];
 
-        for (let i = 0; i < 11; i++) {
-            ie.push(parseInt(inscricao[i]) * peso[i]);
-        }
+            for (let i = 0; i < 11; i++) {
+                ie.push(parseInt(inscricao[i]) * peso[i]);
+            }
 
-        const somaProdutos = ie.reduce(function (acumulador, valor) {
-            return acumulador + valor;
-        }, 0);
+            const somaProdutos = ie.reduce(function (acumulador, valor) {
+                return acumulador + valor;
+            }, 0);
 
-        const resto = somaProdutos % 11;
+            const resto = somaProdutos % 11;
 
-        digitoVerificador1 = 11 - resto;
+            digitoVerificador1 = 11 - resto;
 
-        if (digitoVerificador1 === 10 || digitoVerificador1 === 11) {
-            digitoVerificador1 = 0;
-        }
+            if (digitoVerificador1 === 10 || digitoVerificador1 === 11) {
+                digitoVerificador1 = 0;
+            }
 
-        // segundo cálculo 
+            // segundo cálculo 
+            const peso2 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+            const ie2 = [];
 
-        const peso2 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
-        const ie2 = [];
+            for (let i = 0; i < 12; i++) {
+                ie2.push(parseInt(inscricao[i]) * peso2[i]);
+            }
 
-        for (let i = 0; i < 12; i++) {
-            ie2.push(parseInt(inscricao[i]) * peso2[i]);
-        }
+            const somaProdutos2 = ie2.reduce(function (acumulador, valor) {
+                return acumulador + valor;
+            }, 0);
 
-        const somaProdutos2 = ie2.reduce(function (acumulador, valor) {
-            return acumulador + valor;
-        }, 0);
+            const resto2 = somaProdutos2 % 11;
 
-        const resto2 = somaProdutos2 % 11;
+            digitoVerificador2 = 11 - resto2;
 
-        digitoVerificador2 = 11 - resto2;
+            if (digitoVerificador2 === 10 || digitoVerificador2 === 11) {
+                digitoVerificador2 = 0;
+            }
 
-        if (digitoVerificador2 === 10 || digitoVerificador2 === 11) {
-            digitoVerificador2 = 0;
-        }
+            if (inscricao[11] == digitoVerificador1 && inscricao[12] == digitoVerificador2) {
+                return true
 
-        if (inscricao[11] == digitoVerificador1 && inscricao[12] == digitoVerificador2) {
-            //console.log('sim')
-            true
+            } else {
+                return false
+            }
+            
         } else {
-            //console.log('não')
-            false
+            return false
         }
+
     } else {
-        false
+        return false
     }
 
-} else {
-    false
 }
 
-
+//let inscricao = "01.004.823/001-12";
+//console.log(validaIEAC(inscricao))
