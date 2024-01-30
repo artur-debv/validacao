1ee986fdda4bc799ab9588c5b12ba3c50cd77798
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-30 11:20:47 -0300

    Update validaIESE.js

--- valida ie/validaIESE.js
+++ valida ie/validaIESE.js
@@ -1,37 +1,42 @@
-let inscricao = "27123456-3";
+function validaIESE() {
 
-inscricao = inscricao.replace(/\D/g, '');
+    inscricao = inscricao.replace(/\D/g, '');
 
-const validainscricao = /^[0-9]{9}$/.test(inscricao);
+    const validainscricao = /^[0-9]{9}$/.test(inscricao);
 
-if (validainscricao) {
-    const peso = [9, 8, 7, 6, 5, 4, 3, 2];
-    const ie = [];
+    if (validainscricao) {
 
-    for (let i = 0; i < 8; i++) {
-        ie.push(parseInt(inscricao[i]) * peso[i]);
-    }
+        const peso = [9, 8, 7, 6, 5, 4, 3, 2];
+        const ie = [];
 
-    const somaProdutos = ie.reduce(function (acumulador, valor) {
-        return acumulador + valor;
-    }, 0);
+        for (let i = 0; i < 8; i++) {
+            ie.push(parseInt(inscricao[i]) * peso[i]);
+        }
 
-    const resto = somaProdutos % 11;
+        const somaProdutos = ie.reduce(function (acumulador, valor) {
+            return acumulador + valor;
+        }, 0);
 
-    if (resto === 0 || resto === 1) {
-        digitoVerificador = 0;
+        const resto = somaProdutos % 11;
+
+        if (resto === 0 || resto === 1) {
+            digitoVerificador = 0;
+        } else {
+            digitoVerificador = 11 - resto;
+        }
+
+        if (inscricao[8] == digitoVerificador) {
+            return true
+
+        } else {
+            return false
+        }
+
     } else {
-        digitoVerificador = 11 - resto;
+        return false
     }
 
-    if (inscricao[8] == digitoVerificador) {
-        //console.log('sim')
-        true
-    } else {
-        //console.log('não')
-        false
-    }
-
-} else {
-    false
 }
+
+//let inscricao = "27123456-3";
+//console.log(validaIESE(inscricao))
