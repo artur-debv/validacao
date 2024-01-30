5585433ee8bb392797886187a08009b281a8eea2
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-30 11:20:06 -0300

    Update validaIERN.js

--- valida ie/validaIERN.js
+++ valida ie/validaIERN.js
@@ -1,88 +1,105 @@
-let inscricao = "20.0.040.040-0";
+function validaIERN() {
 
-inscricao = inscricao.replace(/\D/g, '');
+  inscricao = inscricao.replace(/\D/g, '');
 
-if (inscricao.length === 9) {
-  const validainscricao = /^[0-9]{9}$/.test(inscricao);
-  if (validainscricao) {
-    if(inscricao[2] == 2 && inscricao[0] == 0){
-       // primeiro cálculo
+  if (inscricao.length === 9) {
 
-    const peso = [9,8,7,6,5,4,3,2]; 
-    const ie2 = [];
+    const validainscricao = /^[0-9]{9}$/.test(inscricao);
 
+    if (validainscricao) {
 
-    for (let i = 0; i < 8; i++) {
-     ie2.push(parseInt(inscricao[i]) * peso[i]);
-    }
+      if (inscricao[0] == 2 && inscricao[1] == 0) {
 
+        const peso = [9, 8, 7, 6, 5, 4, 3, 2];
+        const ie = [];
 
-    const somaProdutos = ie2.reduce(function (acumulador, valor) {
-    return acumulador + valor;
-    }, 0);
+        for (let i = 0; i < 8; i++) {
+          ie.push(parseInt(inscricao[i]) * peso[i]);
+        }
 
+        const somaProdutos = ie.reduce(function (acumulador, valor) {
+          return acumulador + valor;
+        }, 0);
 
-    const produto = somaProdutos * 10
-    const resto = produto % 11;
+        const produto = somaProdutos * 10
 
-    let digitoVerificador
-    if (resto === 10) {
-    digitoVerificador = 0;
+        const resto = produto % 11;
+
+        if (resto === 10) {
+          digitoVerificador = 0;
+
+        } else {
+          digitoVerificador = resto;
+        }
+
+        if (inscricao[8] == digitoVerificador) {
+          return true
+
+        } else {
+          return false
+        }
+
+      } else {
+        return false
+      }
+
     } else {
-    digitoVerificador = resto;
+      return false
     }
 
-    console.log(produto)
+  } else if (inscricao.length === 10) {
 
-  } else {
-    console.log("Inscrição inválida");
-  }
+    const validainscricao = /^[0-9]{10}$/.test(inscricao);
 
-} else if (inscricao.length === 10) {
-  const validainscricao = /^[0-9]{10}$/.test(inscricao);
+    if (validainscricao) {
 
-  if (validainscricao) {
+      if (inscricao[0] == 2 && inscricao[1] == 0) {
 
-    if(inscricao[2] == 2 && inscricao[0] == 0){
-      // segundo cálculo 
+        const peso2 = [10, 9, 8, 7, 6, 5, 4, 3, 2];
+        const ie2 = [];
 
-    const peso1 = [10, 9, 8, 7, 6, 5, 4, 3, 2];
-    const ie4 = [];
-    
-    for (let i = 0; i < 8; i++) {
-      ie4.push(parseInt(inscricao[i]) * peso1[i]);
-    }
-    
-    const somaProdutos1 = ie4.reduce(function (acumulador, valor) {
-      return acumulador + valor;
-    }, 0);
-    
-    const produto1 = somaProdutos1 * 10; // Multiplicação da soma total por 10
-    const resto1 = produto1 % 11;
-    
-    let digitoVerificador1;
-    if (resto1 === 10) {
-      digitoVerificador1 = 0;
+        for (let i = 0; i < 8; i++) {
+          ie2.push(parseInt(inscricao[i]) * peso2[i]);
+        }
+
+        const somaProdutos2 = ie2.reduce(function (acumulador, valor) {
+          return acumulador + valor;
+        }, 0);
+
+        const produto2 = somaProdutos2 * 10;
+
+        const resto2 = produto2 % 11;
+
+        if (resto2 === 10) {
+          digitoVerificador = 0;
+
+        } else {
+          digitoVerificador = resto2;
+        }
+
+        if (inscricao[9] == digitoVerificador) {
+          return true
+
+        } else {
+          return false
+        }
+
+      } else {
+        return false
+      }
+
     } else {
-      digitoVerificador1 = resto1;
-    }
-    
-    console.table(ie4)
-    console.log(produto1)
-
-    if(inscricao[8] == digitoVerificador && inscricao[9] == digitoVerificador1){
-      // console.log("sim")
-      return true
-    }else{
-      // console.log("não")
       return false
     }
 
   } else {
-    false
+    return false
   }
-    }
-    }
-} else {
-  false
+
 }
+
+//let inscricao = "20.040.040-1";
+//let inscricao = "20.0.040.040-0";
+//console.log(validaIERN(inscricao))
+
+
