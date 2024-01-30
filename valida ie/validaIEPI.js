f7607f6d95dd80b7b4c8df4f53f144ca3f64a4b9
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-30 11:19:45 -0300

    Update validaIEPI.js

--- valida ie/validaIEPI.js
+++ valida ie/validaIEPI.js
@@ -1,36 +1,41 @@
-let inscricao = "012345679";
+function validaIEPI() {
 
-inscricao = inscricao.replace(/\D/g, '');
+    inscricao = inscricao.replace(/\D/g, '');
 
-const validainscricao = /^[0-9]{9}$/.test(inscricao);
+    const validainscricao = /^[0-9]{9}$/.test(inscricao);
 
-if (validainscricao) {
-    const peso = [9, 8, 7, 6, 5, 4, 3, 2];
-    const ie = [];
+    if (validainscricao) {
+        
+        const peso = [9, 8, 7, 6, 5, 4, 3, 2];
+        const ie = [];
 
-    for (let i = 0; i < 8; i++) {
-        ie.push(parseInt(inscricao[i]) * peso[i]);
-    }
+        for (let i = 0; i < 8; i++) {
+            ie.push(parseInt(inscricao[i]) * peso[i]);
+        }
 
-    const somaProdutos = ie.reduce(function (acumulador, valor) {
-        return acumulador + valor;
-    }, 0);
+        const somaProdutos = ie.reduce(function (acumulador, valor) {
+            return acumulador + valor;
+        }, 0);
 
-    const resto = somaProdutos % 11;
-    digitoVerificador = 11 - resto;
+        const resto = somaProdutos % 11;
 
-    if (digitoVerificador === 10 || digitoVerificador === 11) {
-        digitoVerificador = 0;
-    }
+        digitoVerificador = 11 - resto;
 
-    if (inscricao[8] == digitoVerificador) {
-        //console.log('sim')
-        true
+        if (digitoVerificador === 10 || digitoVerificador === 11) {
+            digitoVerificador = 0;
+        }
+
+        if (inscricao[8] == digitoVerificador) {
+            return true
+
+        } else {
+            return false
+        }
+
     } else {
-        //console.log('não')
-        false
+        return false
     }
 
-} else {
-    false
 }
+//let inscricao = "012345679";
+//console.log(validaIEPI(inscricao))
