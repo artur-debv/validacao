75e31748b1858ce89a8dd7fb4001895c7f21a469
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-30 11:19:54 -0300

    Update validaIEPR.js

--- valida ie/validaIEPR.js
+++ valida ie/validaIEPR.js
@@ -1,63 +1,64 @@
-let inscricao = "123.45678-50";
+function validaIEPR() {
 
-inscricao = inscricao.replace(/\D/g, '');
+    inscricao = inscricao.replace(/\D/g, '');
 
-const validainscricao = /^[0-9]{10}$/.test(inscricao);
+    const validainscricao = /^[0-9]{10}$/.test(inscricao);
 
-if (validainscricao) {
+    if (validainscricao) {
 
-    // primeiro cálculo //
+        // primeiro cálculo //
+        const pesos = [3, 2, 7, 6, 5, 4, 3, 2];
+        let ie = [];
 
-    const pesos = [3, 2, 7, 6, 5, 4, 3, 2];
-    let ie = [];
+        for (let i = 0; i < 8; i++) {
+            ie.push(parseInt(inscricao[i]) * pesos[i]);
+        }
 
+        const somaProdutos = ie.reduce(function (acumulador, valor) {
+            return acumulador + valor;
+        }, 0)
 
-    for (let i = 0; i < 8; i++) {
-        ie.push(parseInt(inscricao[i]) * pesos[i]);
-    }
+        const resto = somaProdutos % 11;
 
-    const somaProdutos = ie.reduce(function (acumulador, valor) {
-        return acumulador + valor;
-    }, 0)
+        if (resto === 0 || resto === 1) {
+            digitoVerificador = 0;
+        } else {
+            digitoVerificador = 11 - resto;
+        }
 
-    const resto = somaProdutos % 11;
+        // segundo cálculo
+        const pesos2 = [4, 3, 2, 7, 6, 5, 4, 3, 2];
+        let ie2 = [];
 
-    if (resto === 0 || resto === 1) {
-        digitoVerificador = 0;
+        for (let i = 0; i < 9; i++) {
+            ie2.push(parseInt(inscricao[i]) * pesos2[i]);
+        }
+
+        const somaProdutos2 = ie2.reduce(function (acumulador, valor) {
+            return acumulador + valor;
+        }, 0)
+
+        const resto2 = somaProdutos2 % 11;
+
+        if (resto2 === 0 || resto2 === 1) {
+            digitoVerificador2 = 0;
+            
+        } else {
+            digitoVerificador2 = 11 - resto2;
+        }
+
+        if (inscricao[8] == digitoVerificador && inscricao[9] == digitoVerificador2) {
+            return true
+
+        } else {
+            return false
+        }
+
     } else {
-        digitoVerificador = 11 - resto;
+        return false
     }
 
-    // segundo cálculo
-
-    const pesos2 = [4, 3, 2, 7, 6, 5, 4, 3, 2];
-    let ie2 = [];
-
-
-    for (let i = 0; i < 9; i++) {
-        ie2.push(parseInt(inscricao[i]) * pesos2[i]);
-    }
-
-    const somaProdutos2 = ie2.reduce(function (acumulador, valor) {
-        return acumulador + valor;
-    }, 0)
-
-    const resto2 = somaProdutos2 % 11;
-
-    if (resto2 === 0 || resto2 === 1) {
-        digitoVerificador2 = 0;
-    } else {
-        digitoVerificador2 = 11 - resto2;
-    }
-
-    if (inscricao[8] == digitoVerificador && inscricao[9] == digitoVerificador2) {
-        //console.log('sim')
-        true
-    } else {
-        //console.log('não')
-        false
-    }
-
-} else {
-    false
 }
+
+//let inscricao = "123.45678-50";
+//console.log(validaIEPR(inscricao))
