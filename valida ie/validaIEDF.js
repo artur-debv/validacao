02dbb85741716e440cd2bb4385ef7728b6bcb500
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-30 11:18:28 -0300

    Update validaIEDF.js

--- valida ie/validaIEDF.js
+++ valida ie/validaIEDF.js
@@ -1,60 +1,63 @@
-let inscricao = '07 300001 001 - 09';
+function validaIEDF() {
 
-inscricao = inscricao.replace(/\D/g, '');
+    inscricao = inscricao.replace(/\D/g, '');
 
-const validainscricao = /^[0-9]{13}$/.test(inscricao);
+    const validainscricao = /^[0-9]{13}$/.test(inscricao);
 
-if (validainscricao) {
-    // primeiro cálculo //
+    if (validainscricao) {
 
-    const pesos = [4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
-    let ie = [];
+        // primeiro cálculo //
+        const pesos = [4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+        let ie = [];
 
+        for (let i = 0; i < 11; i++) {
+            ie.push(parseInt(inscricao[i]) * pesos[i]);
+        }
 
-    for (let i = 0; i < 11; i++) {
-        ie.push(parseInt(inscricao[i]) * pesos[i]);
-    }
+        const somaProdutos = ie.reduce(function (acumulador, valor) {
+            return acumulador + valor;
+        }, 0)
 
-    const somaProdutos = ie.reduce(function (acumulador, valor) {
-        return acumulador + valor;
-    }, 0)
+        const resto = somaProdutos % 11;
 
-    const resto = somaProdutos % 11;
-    digitoVerificador = 11 - resto;
+        digitoVerificador = 11 - resto;
 
-    if (digitoVerificador === 10 || digitoVerificador === 11) {
-        digitoVerificador = 0;
-    }
+        if (digitoVerificador === 10 || digitoVerificador === 11) {
+            digitoVerificador = 0;
+        }
 
-    //segundo cálculo
+        //segundo cálculo
+        const pesos2 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
+        let ie2 = [];
 
-    const pesos2 = [5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2];
-    let ie2 = [];
+        for (let i = 0; i < 12; i++) {
+            ie2.push(parseInt(inscricao[i]) * pesos2[i]);
+        }
 
+        const somaProdutos2 = ie2.reduce(function (acumulador, valor) {
+            return acumulador + valor;
+        }, 0)
 
-    for (let i = 0; i < 12; i++) {
-        ie2.push(parseInt(inscricao[i]) * pesos2[i]);
-    }
+        const resto2 = somaProdutos2 % 11;
 
-    const somaProdutos2 = ie2.reduce(function (acumulador, valor) {
-        return acumulador + valor;
-    }, 0)
+        digitoVerificador2 = 11 - resto2;
 
-    const resto2 = somaProdutos2 % 11;
-    digitoVerificador2 = 11 - resto2;
+        if (digitoVerificador2 === 10 || digitoVerificador2 === 11) {
+            digitoVerificador2 = 0;
+        }
 
-    if (digitoVerificador2 === 10 || digitoVerificador2 === 11) {
-        digitoVerificador2 = 0;
-    }
-    
-    if (inscricao[11] == digitoVerificador && inscricao[12] == digitoVerificador2) {
-        //console.log('sim')
-        true
+        if (inscricao[11] == digitoVerificador && inscricao[12] == digitoVerificador2) {
+            return true
+
+        } else {
+            return false
+        }
+
     } else {
-        //console.log('não')
-        falses
+        return false
     }
 
-} else {
-    false
 }
+
+//let inscricao = "07 300001 001 - 09";
+//console.log(validaIEDF(inscricao))
