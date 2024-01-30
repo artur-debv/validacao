67f95846f92131b5bd690e01a30759e2758d1f95
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-30 11:18:48 -0300

    Update validaIEMA.js

--- valida ie/validaIEMA.js
+++ valida ie/validaIEMA.js
@@ -1,45 +1,49 @@
-let inscricao = "120000385";
+function validaIEMA() {
 
-inscricao = inscricao.replace(/\D/g, '');
+    inscricao = inscricao.replace(/\D/g, '');
 
-const validainscricao = /^[0-9]{9}$/.test(inscricao);
+    const validainscricao = /^[0-9]{9}$/.test(inscricao);
 
-if (validainscricao) {
+    if (validainscricao) {
 
-    if (inscricao[0] == 1 && inscricao[1] == 2) {
-        const peso = [9, 8, 7, 6, 5, 4, 3, 2];
-        const ie = [];
+        if (inscricao[0] == 1 && inscricao[1] == 2) {
+            const peso = [9, 8, 7, 6, 5, 4, 3, 2];
+            const ie = [];
 
-        for (let i = 0; i < 8; i++) {
-            ie.push(parseInt(inscricao[i]) * peso[i]);
-        }
+            for (let i = 0; i < 8; i++) {
+                ie.push(parseInt(inscricao[i]) * peso[i]);
+            }
 
-        const somaProdutos = ie.reduce(function (acumulador, valor) {
-            return acumulador + valor;
-        }, 0);
+            const somaProdutos = ie.reduce(function (acumulador, valor) {
+                return acumulador + valor;
+            }, 0);
 
-        const resto = somaProdutos % 11;
+            const resto = somaProdutos % 11;
 
-        if (resto === 0 || resto === 1) {
-            digitoVerificador = 0;
+            if (resto === 0 || resto === 1) {
+                digitoVerificador = 0;
+
+            } else {
+                digitoVerificador = 11 - resto;
+            }
+
+            if (inscricao[8] == digitoVerificador) {
+                return true
+
+            } else {
+                return false
+            }
 
         } else {
-            digitoVerificador = 11 - resto;
+            return false
         }
 
-        if (inscricao[8] == digitoVerificador) {
-            //console.log('sim')
-            true
-        } else {
-            //console.log('não')
-            false
-        }
     } else {
-        false
+        return false
     }
 
-} else {
-    false
 }
 
 
+//let inscricao = "120000385";
+//console.log(validaIEMA(inscricao))
