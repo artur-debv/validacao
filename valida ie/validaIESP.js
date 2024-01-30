606bf95951d39a4a6c14b43edf5d36cb77f4f747
fatimadachari <fatima.s.dachari@gmail.com>
2024-01-30 14:24:47 -0300

    Update validaIESP.js

--- valida ie/validaIESP.js
+++ valida ie/validaIESP.js
@@ -1,109 +1,110 @@
-let inscricao = "110.042.490.114";
+function validaIESP() {
 
-inscricao = inscricao.replace(/\D/g, '');
+  function reverseNumber(num) {
+    return reversedString = num.toString().split('').reverse().join('');
+    //return parseInt(reversedString) * Math.sign(num);
+  }
 
-if (inscricao.length === 12) {
-  const validainscricao = /^[0-9]{12}$/.test(inscricao);
+  inscricao = inscricao.replace(/[^0-9pP]/g, '');
 
-  if (validainscricao) {
-    // primeiro cálculo //
+  if (inscricao.length === 12) {
 
-    const pesos1 = [1, 3, 4, 5, 6, 7, 8, 10];
+    const validainscricao = /^[0-9]{12}$/.test(inscricao);
 
-    let ie = [];
+    if (validainscricao) {
+
+      // primeiro cálculo //
+      const pesos = [1, 3, 4, 5, 6, 7, 8, 10];
+      let ie = [];
 
 
-    for (let i = 0; i < 8; i++) {
-      ie.push(parseInt(inscricao[i]) * pesos1[i]);
+      for (let i = 0; i < 8; i++) {
+        ie.push(parseInt(inscricao[i]) * pesos[i]);
+      }
+
+      const somaProdutos = ie.reduce(function (acumulador, valor) {
+        return acumulador + valor;
+      }, 0)
+
+      const resto = somaProdutos % 11;
+
+      const invertedNumber = reverseNumber(resto);
+
+      const digitoVerificador = invertedNumber[0];
+
+      // segundo cálculo
+      const pesos2 = [3, 2, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+      let ie2 = [];
+
+
+      for (let i = 0; i < 11; i++) {
+        ie2.push(parseInt(inscricao[i], 10) * pesos2[i]);
+      }
+
+      const somaProdutos2 = ie2.reduce(function (acumulador, valor) {
+        return acumulador + valor;
+      }, 0)
+
+      const resto2 = somaProdutos2 % 11;
+
+      const invertedNumber2 = reverseNumber(resto2);
+
+      const digitoVerificador2 = invertedNumber2[0];
+
+      if (inscricao[8] == digitoVerificador && inscricao[4] == digitoVerificador2) {
+        return true
+
+      } else {
+        return false
+      }
+
+    } else {
+      console.log('nao 1')
+      return false
     }
 
-    console.table(ie);
+  } else if (inscricao.length === 13) {
 
-    const somaProdutos1 = ie.reduce(function (acumulador, valor) {
-      return acumulador + valor;
-    }, 0)
+    const validainscricao = /^[pP]?[0-9]{12}$/.test(inscricao);
 
-    const resto = somaProdutos1 % 11;
+    if (validainscricao) {
+      inscricao = inscricao.replace(/[^0-9]/g, '');
 
-    const restoString = resto.toString()
+      const pesos = [1, 3, 4, 5, 6, 7, 8, 10];
+      let ie = [];
 
-    console.log(restoString.charAt(1));
+      for (let i = 0; i < 8; i++) {
+        ie.push(parseInt(inscricao[i]) * pesos[i]);
+      }
 
+      const somaProdutos = ie.reduce(function (acumulador, valor) {
+        return acumulador + valor;
+      }, 0);
 
-    // segundo cálculo
+      const resto = somaProdutos % 11;
 
-    let ie2 = [];
+      const invertedNumber = reverseNumber(resto);
 
-    const pesos2 = [3, 2, 10, 9, 8, 7, 6, 5, 4, 3, 2];
+      const digitoVerificador = invertedNumber[0];
 
-    for (let i = 0; i < 11; i++) {
-      ie2.push(parseInt(inscricao[i], 10) * pesos2[i]);
-    }
+      if (inscricao[8] == digitoVerificador) {
+        return true
+      } else {
 
-    const somaProdutosb = ie2.reduce(function (acumulador, valor) {
-      return acumulador + valor;
-    }, 0)
+        return false
+      }
 
-    const resto2 = somaProdutosb % 11;
-
-    const restoString1 = resto2.toString()
-
-    console.log(restoString1)
-
-
-    const digitoverificador1 = resto[1]
-    const digitoverificador2 = resto[1]
-
-    if (ie[8] == digitoverificador1 && ie[4] == digitoverificador2) {
-      return true
     } else {
       return false
     }
 
   } else {
-    console.log("Inscrição inválida");
+    console.log('nao')
+    return false
   }
 
-} else if (inscricao.length === 13) {
-  const validainscricao = /^[0-9]{13}$/.test(inscricao);
-
-  if (validainscricao) {
-    // primeiro cálculo  //
-
-    const pesos3 = [1, 3, 4, 5, 6, 7, 8, 10];
-
-    
-    let ie3 = [];
-
-    for (let i = 0; i < 8; i++) {
-      ie3.push(parseInt(inscricao[i]) * pesos3[i]);
-    }
-
-    console.table(ie3);
-
-    const somaProdutosc = ie3.reduce(function (acumulador, valor) {
-      return acumulador + valor;
-    }, 0);
-
-    const resto3 = somaProdutosc % 11;
-
-    const restoString3 = resto3.toString()
-
-    console.log(restoString3)
-
-    const digitoverificador3 = resto3.charAt(1)
-
-
-    if (ie[8] == digitoverificador3) {
-      return true
-    } else {
-      return false
-    }
-
-  } else {
-    false
-  }
-
-} else {
-  false
 }
+
+//let inscricao = "110.042.490.114";
+//let inscricao = "P-01100424.3/002";
+//console.log(validaIESP(inscricao))
